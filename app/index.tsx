79666c2d09c54519ff197c48a87d6c7e1acdd4b7
Max <camilamachadodesouza77@gmail.com>
2026-08-24 19:27:38 -0300

    @MaxPichy

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,25 @@
 import { Text, View, StyleSheet } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { externalStyles } from './styles';
 
 export default function Home(){
-    return(
-        <View>
-            <Text>Hello World!</Text>
-            <Text>Welcome User!</Text>     
-        </View>
+    return (
+            <SafeAreaView>
+                <View style={style.container}>
+                    <Text style={{borderColor:'red',borderWidth:5}}>Hello, World!</Text> {/* estilo inline */}
+                    <Text style={externalStyles.greenBox}>Wellcome to PDM class!</Text>
+                </View>
+            </SafeAreaView>
     );
 }
 
 const style = StyleSheet.create({
-    meuTexto:{
-        color: 'red',
-        fontSize: 20
+    container:{
+        fontSize: 20,
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderColor: 'blue',
+        borderWidth: 5
     }
 });
---
+++ app/styles.tsx
@@ -0,0 +1,8 @@
+import { StyleSheet } from 'react-native'
+
+export const externalStyles = StyleSheet.create({
+    greenBox: {
+        borderColor: 'purple',
+        borderWidth: 5
+    }
+});
