7567c57908199be4ea0070f225a320f02c642c8f
Max <camilamachadodesouza77@gmail.com>
2026-08-25 22:38:41 -0300

    @MaxPichy

--- app/index.tsx
+++ app/index.tsx
@@ -1,25 +1,45 @@
-import { Text, View, StyleSheet } from 'react-native';
+import { StyleSheet, Text, View, Image, Pressable } from 'react-native';
+import { useState } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { externalStyles } from './styles';
+import { styles } from './styles';
 
-export default function Home(){
-    return (
-            <SafeAreaView>
-                <View style={style.container}>
-                    <Text style={{borderColor:'red',borderWidth:5}}>Hello, World!</Text> {/* estilo inline */}
-                    <Text style={externalStyles.greenBox}>Wellcome to PDM class!</Text>
-                </View>
-            </SafeAreaView>
-    );
-}
 
-const style = StyleSheet.create({
-    container:{
-        fontSize: 20,
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderColor: 'blue',
-        borderWidth: 5
+export default function Home() {
+    function iniciarAplicacao(){
+        console.log('O botão foi pressionado!')
     }
-});
+
+    function changeText(){
+        console.log('O botão foi long pressed!')
+    }
+
+    return(
+        <SafeAreaView style={styles.safeArea}>
+            <View style={styles.container}>
+                <View style={styles.card}>
+
+                    <Image
+                        source={require("../assets/images/logo.png")}
+                        style={styles.logo}
+                        resizeMode='contain'
+                    />
+                    <Text style={styles.titulo}>TaskFlow</Text>
+                    <Text style={styles.descricao}>
+                        Organize sua tarefa de forma simples
+                    </Text>
+                    <Pressable 
+                        onPress={iniciarAplicacao}
+                        onLongPress={changeText} 
+                        style={({pressed}) => [styles.botao, pressed && styles.botaoPressionado]}
+                    >
+                        {({pressed}) => (
+                            <Text style={styles.textoBotao}>
+                                {pressed ? 'Carregando...' : 'Começar'}
+                            </Text>
+                        )}
+                    </Pressable>
+                </View>
+            </View>
+        </SafeAreaView>
+    );
+};
--- app/styles.tsx
+++ app/styles.tsx
@@ -1,8 +1,54 @@
-import { StyleSheet } from 'react-native'
+import { StyleSheet } from "react-native";
 
-export const externalStyles = StyleSheet.create({
-    greenBox: {
-        borderColor: 'purple',
-        borderWidth: 5
+export const styles = StyleSheet.create({
+    safeArea:{
+        flex: 1
+    },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#f3f3f3',
+        padding: 20
+    },
+    titulo:{
+        fontSize: 34,
+        fontWeight: "bold",
+        color: '#2c61b6ff',
+        marginBottom: 10
+    },
+    descricao:{
+        fontSize: 18,
+        textAlign: "center",
+        marginBottom: 20,
+        lineHeight: 26,
+        color: '#555'
+    },
+    logo:{
+        width: 140,
+        height: 140,
+        marginBottom: 20,
+    },
+    card:{
+        backgroundColor: '#ffffff',
+        padding: 30,
+        borderRadius: 20,
+        alignItems: 'center'
+    },
+    botao:{
+        backgroundColor: '#2563EB',
+        paddingVertical: 15,
+        paddingHorizontal: 40,
+        borderRadius: 10
+    },
+    textoBotao:{
+        color: '#fff',
+        fontWeight: 'bold',
+        fontSize: 18
+    },
+    botaoPressionado:{
+        backgroundColor: '#173c8dff',
+        color: '#9f9f9fff',
+        transform: 'scale(1.2)',
     }
 });
