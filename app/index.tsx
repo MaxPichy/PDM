6dbc2109bc086d6fc17c1a697707f87c7e1ea3e0
Max <camilamachadodesouza77@gmail.com>
2026-09-01 22:40:59 -0300

    useState / useEffect

--- app/index.tsx
+++ app/index.tsx
@@ -1,45 +1,34 @@
-import { StyleSheet, Text, View, Image, Pressable } from 'react-native';
 import { useState } from 'react';
+import { Image, Pressable, Text, View, Button } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { styles } from './styles';
 
-
 export default function Home() {
-    function iniciarAplicacao(){
-        console.log('O botão foi pressionado!')
+    const [contador, setContador] = useState(0);
+
+    function incrementar(){
+        setContador(contador+1);
     }
 
-    function changeText(){
-        console.log('O botão foi long pressed!')
+    function decrementar(){
+        if(contador > 0){
+            setContador(contador-1);
+        }
     }
 
-    return(
+    return (
         <SafeAreaView style={styles.safeArea}>
             <View style={styles.container}>
                 <View style={styles.card}>
-
-                    <Image
-                        source={require("../assets/images/logo.png")}
-                        style={styles.logo}
-                        resizeMode='contain'
-                    />
-                    <Text style={styles.titulo}>TaskFlow</Text>
-                    <Text style={styles.descricao}>
-                        Organize sua tarefa de forma simples
+                    <Text>CONTADOR</Text>
+                    <Text style={{fontSize: 25}}>
+                        {contador}
                     </Text>
-                    <Pressable 
-                        onPress={iniciarAplicacao}
-                        onLongPress={changeText} 
-                        style={({pressed}) => [styles.botao, pressed && styles.botaoPressionado]}
-                    >
-                        {({pressed}) => (
-                            <Text style={styles.textoBotao}>
-                                {pressed ? 'Carregando...' : 'Começar'}
-                            </Text>
-                        )}
-                    </Pressable>
+
+                    <Button title='+' onPress={incrementar}></Button>
+                    <Button title='-' onPress={decrementar}></Button>
                 </View>
             </View>
         </SafeAreaView>
     );
-};
+}
